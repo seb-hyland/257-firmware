c7c8dd3f12466acbba3a3a48ce888e6e55683d95
Sebastian Hyland <st.hyland05@gmail.com>
2025-03-06 09:18:50 -0800

    feat(setup): added setup event loop logic and TUI

--- main.ino
+++ main.ino
@@ -1,60 +1,85 @@
 int medConfig[10][2];
 
 void setup() {
-  Serial.begin(9600);
-  int i = 0;
+    Serial.begin(9600);
+    int i = 0;
   
-  while ((Serial.available() && Serial.read() != 'f') && i < 10) {
-    init_dosage(i);
-    init_timing(i);
-    i++;
-  }
+    while (i < 10) {
+	init_dosage(i);
+	init_timing(i);
+	i++;
+
+	if (i == 9) {
+	    break;
+	}
+
+	Serial.println("Press a key to continue:");
+	Serial.println(" ——————————————————————————————————————————————— ");
+	Serial.println("| Key | Description                             |");
+	Serial.println("|—————|—————————————————————————————————————————|");
+	Serial.println("|  c  | Continue setting up another medication  |");
+	Serial.println("|  f  | Finish calibration                      |");
+	Serial.println(" ——————————————————————————————————————————————— ");
+
+	char input;
+	while (true) {
+	    if (Serial.available() > 0) {
+		input = Serial.read();
+		if (input == 'f' || input == 'c') {
+		    break;
+		}
+	    }
+	}
+	if (input == 'f') {
+	    break;
+	}
+    }
 } 
 
 void loop() {
-  unsigned long currentTime = millis();
+    unsigned long currentTime = millis();
 
-  // TODO
+    // TODO
 }
 
 
 void init_dosage(int i) {
-  // TODO: modifies medConfig[i][0] through serial TUI
+    // TODO: modifies medConfig[i][0] through serial TUI
 }
 
 void init_timing(int i) {
-  // TODO: modifies medConfig[i][1] through serial TUI
+    // TODO: modifies medConfig[i][1] through serial TUI
 }
 
 
 void alert_visual() {
-  // TODO
+    // TODO
 }
 
 void alert_verbal() {
-  // TODO
+    // TODO
 }
 
 void measure_motion() {
-  // TODO
+    // TODO
 }
 
 void check_motion_threshold() {
-  // TODO
+    // TODO
 }
 
 void move_platform() {
-  // TODO
+    // TODO
 }
 
 void move_arm() {
-  // TODO
+    // TODO
 }
 
 void dispense_medication() {
-  // TODO
+    // TODO
 }
 
 void alert_caregiver() {
-  // TODO
+    // TODO
 }
